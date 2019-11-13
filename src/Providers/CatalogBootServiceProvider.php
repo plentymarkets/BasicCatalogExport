2eb4a3369d2224a04f6f8aa9ec3b9b677a4416f6
Henry Malicha <henry.malicha@plentymarkets.com>
2019-11-13 14:17:10 +0100

    code cleanup

--- src/Providers/CatalogBootServiceProvider.php
+++ src/Providers/CatalogBootServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace BasicCatalogExport\Providers;
 
-use BasicCatalogExport\DataProviders\FirstBaseDataProvider;
-use BasicCatalogExport\DataProviders\FirstKeyDataProvider;
-use BasicCatalogExport\DataProviders\FirstNestedKeyDataProvider;
 use Plenty\Modules\Catalog\Contracts\TemplateContainerContract;
 use Plenty\Plugin\ServiceProvider;
 
@@ -15,9 +12,6 @@ class CatalogBootServiceProvider extends ServiceProvider
      */
     public function boot(TemplateContainerContract $container) {
         // Creating a new template, The provider class is responsible for the booting process
-        $template = $container->register(BasicCatalogExportServiceProvider::PLUGIN_NAME, 'exampleType', BasicCatalogExportTemplateProvider::class);
-
-        // Id of the template
-        $id = $template->getIdentifier();
+        $container->register(BasicCatalogExportServiceProvider::PLUGIN_NAME, 'exampleType', BasicCatalogExportTemplateProvider::class);
     }
 }

23a39028e9af2a58d4f55d6ffe7bbf5b646e670b
Henry Malicha <henry.malicha@plentymarkets.com>
2021-05-18 12:48:43 +0200

    simple template fields

--- src/Providers/ExampleTemplateProvider.php
+++ src/Providers/ExampleTemplateProvider.php
@@ -5,6 +5,7 @@ namespace BasicCatalogExport\Providers;
 use Plenty\Modules\Catalog\Containers\Filters\CatalogFilterBuilderContainer;
 use Plenty\Modules\Catalog\Containers\TemplateGroupContainer;
 use Plenty\Modules\Catalog\Models\SimpleTemplateField;
+use Plenty\Modules\Catalog\Models\TemplateGroup;
 use Plenty\Modules\Catalog\Templates\Providers\AbstractGroupedTemplateProvider;
 
 /**
@@ -18,12 +19,44 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
         /** @var TemplateGroupContainer $templateGroupContainer */
         $templateGroupContainer = pluginApp(TemplateGroupContainer::class);
 
-        /** @var SimpleTemplateField $templateField */
-        $templateField = pluginApp(SimpleTemplateField::class, [
-            'uniqueIdentifier',
-            'theResultKey',
-            'First example field' // In a productive plugin this should be translated
+        /** @var TemplateGroup $templateGroup */
+        $templateGroup = pluginApp(TemplateGroup::class,
+            [
+                "identifier" => "groupOne",
+                "label" => "fields" // In a productive plugin this should be translated
+            ]);
+
+        /** @var SimpleTemplateField $name */
+        $name = pluginApp(SimpleTemplateField::class, [
+            'variationName',
+            'name',
+            'Variation name', // In a productive plugin this should be translated
+            true
         ]);
+
+        /** @var SimpleTemplateField $price */
+        $price = pluginApp(SimpleTemplateField::class, [
+            'price',
+            'price',
+            'Sales price', // In a productive plugin this should be translated
+            true
+        ]);
+
+        /** @var SimpleTemplateField $sku */
+        $sku = pluginApp(SimpleTemplateField::class, [
+            'sku',
+            'sku',
+            'SKU', // In a productive plugin this should be translated
+            true
+        ]);
+
+        $templateGroup->addGroupField($name);
+        $templateGroup->addGroupField($price);
+        $templateGroup->addGroupField($sku);
+
+        $templateGroupContainer->addGroup($templateGroup);
+
+        return $templateGroupContainer;
     }
 
     public function getFilterContainer(): CatalogFilterBuilderContainer
@@ -34,5 +67,10 @@ class ExampleTemplateProvider extends AbstractGroupedTemplateProvider
     public function getCustomFilterContainer(): CatalogFilterBuilderContainer
     {
         return new CatalogFilterBuilderContainer();
+    }
+
+    public function isPreviewable(): bool
+    {
+        return true;
     }
 }
